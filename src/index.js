38b64078f44bc5707994a5f427762d0664d03f99
unknown <eluewisdom@gmail.com>
2022-01-21 13:05:54 -0800

    changes

--- src/index.js
+++ src/index.js
@@ -8,11 +8,26 @@ import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../src/reducers/rootReducer';
 import reduxPromise from 'redux-promise'
 
-const theStore = applyMiddleware(reduxPromise)(createStore)(rootReducer)
+//Redux persist
+import { persistStore, persistReducer  } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; //defaults to localStorage for web
+import { PersistGate } from 'redux-persist/integration/react';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['siteModal']
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+const theStore = applyMiddleware(reduxPromise)(createStore)(persistedReducer)
+const persistor = persistStore(theStore)
 
 ReactDOM.render(
   <Provider store={theStore}>
-    <App />,
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>,
   </Provider>,
   document.getElementById('root')
 );
