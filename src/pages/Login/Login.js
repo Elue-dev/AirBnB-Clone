6c0eab5422e48ac3e8b31843edd8431d7a1c4081
unknown <eluewisdom@gmail.com>
2022-01-25 14:54:50 -0800

    changes

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -7,11 +7,17 @@ import swal from 'sweetalert';
 import regAction from '../../actions/regAction'
 import axios from 'axios';
 import './Login.css';
+import Spinner from '../../utility/Spinner/Spinner';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core'
+library.add(faSpinner);
 
 function Login(){
 
     const [email, setEmail] = useState('')
     const [password, setpassword] = useState('')
+    const [loading, setLoading] = useState(null)
     const dispatch = useDispatch()
 
     const linkSignUp = () => {
@@ -31,6 +37,11 @@ function Login(){
     }
 
     const submitLogin = async (e) => {
+       
+        window.setTimeout(()=> {
+            setLoading(true)
+          }, 600)
+
         e.preventDefault()
         const url = `${baseUrl}/users/login`
         const data = {
@@ -39,43 +50,46 @@ function Login(){
         }
 
         const resp = await axios.post(url, data)
+
         const token = resp.data.token
-        console.log(token)
-        console.log(resp.data)
+        console.log(token);
+        console.log(resp.data);
+        setLoading(token)
 
-        if(email === '' || password === ''){
-            swal({
-                title: "Please fill in your details",
-                text: "Email and password cannot be blank",
-                icon: "error",
-              })
-              return
-        }
-
-        if(resp.data.msg === 'noEmail'){
-            swal({
-                title: "That email is not registered.",
-                text: "Go to Sign Up and register an email...",
-                icon: "error",
-              })
-        }
-
-        if(resp.data.msg === 'badPass'){
-            swal({
-                title: "Invalid email/password",
-                text: "We don't have a match for that email and password.",
-                icon: "error",
-              })
-        }
-
-        if(resp.data.msg === 'loggedIn'){
-            swal({
-                title: "Login successful",
-                icon: "success",
-              })
-            //   here is where we call our register action to update our auth reducer
-            dispatch(regAction(resp.data)) //sending out everything we got from the server, not just the token
-        }
+                if(email === '' || password === ''){
+                    swal({
+                        title: "Please fill in your details",
+                        text: "Email and password cannot be blank",
+                        icon: "error",
+                      })
+                      return
+                }
+        
+                if(resp.data.msg === 'noEmail'){
+                    swal({
+                        title: "That email is not registered.",
+                        text: "Go to Sign Up and register an email...",
+                        icon: "error",
+                      })
+                }
+        
+                if(resp.data.msg === 'badPass'){
+                    swal({
+                        title: "Invalid email/password",
+                        text: "We don't have a match for that email and password.",
+                        icon: "error",
+                      })
+                      
+                }
+        
+                if(resp.data.msg === 'loggedIn'){
+                    swal({
+                        title: "Login successful",
+                        icon: "success",
+                      })
+                    //   here is where we call our register action to update our auth reducer
+                    dispatch(regAction(resp.data)) //sending out everything we got from the server, not just the token
+                }
     }
 
         return(
@@ -101,7 +115,13 @@ function Login(){
                             </div>
                         </div>
                     </div>
-                    <button className="sign-up-button">Login</button>
+                    {loading ? (
+                        <button className="sign-up-button">
+                            <FontAwesomeIcon icon='spinner' size='1x' spin />
+                        </button>
+                    ) : (
+                        <button className="sign-up-button">Login</button>
+                    )}
                     <div className="divider"></div>
                     <div className='access'>Don't have an account? <span onClick={linkSignUp}>Sign up</span></div>
                 </form>
